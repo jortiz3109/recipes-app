4f5afd98d48a8e5550beeb5d3f85a2c10535afd9
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2023-02-08 19:24:04 -0500

    feat: Add example view

---
+++ resources/views/twitch.blade.php
@@ -0,0 +1,8 @@
+@extends('layouts.app')
+@push('content')
+    <div class="container container-twitch vh-100">
+        <div class="alert alert-success">
+            {{ trans('Hello from twitch') }}
+        </div>
+    </div>
+@endpush
