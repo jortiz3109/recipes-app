4481cfbe3cc91fd47138f588bbea99f66551b556
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2023-02-16 19:54:30 -0500

    feature: Added auth login translations

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -16,5 +16,15 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'login' => [
+        'title' => 'Login',
+        'email' => [
+            'label' => 'Email',
+            'placeholder' => 'john.doe@mail.com'
+        ],
+        'password' => [
+            'label' => 'Password'
+        ],
+    ]
 
 ];
