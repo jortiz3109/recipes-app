7d13edb5a0be69c0a51b187c6264e081e29b8a06
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2023-02-22 20:04:22 -0500

    feat: A authenticated user shouldn't browse on app when is disabled

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware('auth')->group(function () {
+Route::middleware(['auth', 'user.isDisabled'])->group(function () {
     Route::view('home', 'home');
 });
+
+Route::view('/user-is-disabled', 'auth.user_is_disabled');
 
 Route::view('/twitch', 'twitch');
