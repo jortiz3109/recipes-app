8f02dfea7a49b389976e05622edb284b1f46f77c
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2023-03-06 20:01:48 -0500

    Testing vite assets in CI

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,8 +7,4 @@ use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function boot(): void
-    {
-        Vite::macro('images', fn ($asset) => $this->asset("resources/images/{$asset}"));
-    }
 }
