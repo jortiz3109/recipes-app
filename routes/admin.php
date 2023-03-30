8bb8512090945e1f03ea828a184391b95dcff44c
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2023-03-30 08:26:02 -0500

    feat: Admin routing

--- routes/admin.php
+++ routes/admin.php
@@ -3,4 +3,4 @@
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::name('users.index')->get('users', [UserController::class, 'index']);
+Route::resource('users', UserController::class)->only(['index', 'create', 'store']);
