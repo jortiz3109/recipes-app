2fea108675301f33845ead05bd116c91584f6c3f
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2023-04-10 19:39:37 -0500

    test (data-providers): Added test for API data provider

---
+++ tests/Concerns/UseDataProvider.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Concerns;
+
+trait UseDataProvider
+{
+    public function serviceDataProvider(): array
+    {
+        return [
+            'eloquent' => ['provider' => 'eloquent'],
+            'api' => [ 'provider' => 'api']
+        ];
+    }
+}
