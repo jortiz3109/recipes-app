c10df3e0ce009c8efe9a35d1df671093f1f807ac
John Edisson Ortiz Román <j.ortiz3109@gmail.com>
2023-04-12 19:27:43 -0500

    fix: Fixed incorrect aria attribute

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -1,7 +1,7 @@
 <x-admin-index :actions="$actions" :title="$title" :search="$search">
     <div class="card shadow-lg">
         <div class="overflow-x-auto">
-            <table class="table w-full" aria-description="{{ $title }}">
+            <table class="table w-full" aria-label="{{ $title }}">
                 <thead>
                 <tr>
                     <th scope="col">{{ trans('users.name') }}</th>
